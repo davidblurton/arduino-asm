e94dfa31cab6f33570e78d30f69e8f1d7ccc7fef
David Blurton <david.blurton@gmail.com>
2019-10-23 23:19:27 +0000

    Use timer interrupt

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 all:
-	avra hello.asm
+	avra blink.asm
+	# avr-as -mmcu=atmega328p blink.asm
 
-flash:
-	avrdude -p m328p -c stk500v1 -b 57600 -P /dev/cu.wchusbserial1410 -U flash:w:hello.hex
+flash: all
+	avrdude -p m328p -c stk500v1 -b 57600 -P /dev/cu.wchusbserial1420 -U flash:w:blink.hex
+
+dump:
+	avr-objdump -m avr5 -D blink.hex
--- blink.asm
+++ blink.asm
@@ -1,24 +1,46 @@
+;blink.asm
+;  blink an LED which is connected to PB5 (digital out 13)
+
 .include "./m328Pdef.inc"
 
-.DEF A = R16                 ;GENERAL PURPOSE ACCUMULATOR
+.def temp = r16
+.def overflow_counter = r17
+.def led_status = r18
 
-.ORG 0000
-ON_RESET:
-    ldi A, 0b00100000       ;Set PB5 to output
-	out DDRB, A
-    LDI A, 0b00000101       ;SET TIMER PRESCALER TO /1024
-    OUT TCCR0B, A
+.org 0000
+rjmp Setup
 
-MAIN_LOOP:
-    SBI PortB,0              ;FLIP THE 0 BIT
-    RCALL PAUSE             ;WAIT
-    RJMP MAIN_LOOP          ;GO BACK AND DO IT AGAIN
+.org 0x0020
+rjmp TimerOverflow
 
-PAUSE:
-PLUPE:
-    IN A,TIFR0            ;WAIT FOR TIMER
-    ANDI A,0b00000010    ;(1<<TOV0)
-    BREQ PLUPE
-    LDI A,0b00000010     ;RESET FLAG
-    OUT TIFR0,A          ;NOTE: WRITE A 1 (NOT ZERO)
-    RET
+Setup:
+	ldi temp, 0b00100000
+	out DDRB, temp              ; set PB5 to output
+
+    ldi temp,  0b00000101
+    out TCCR0B, temp            ; set the Clock Selector Bits CS00, CS01, CS02 to 101
+                                ; this puts Timer Counter0, TCNT0 in to FCPU/1024 mode
+                                ; so it ticks at the CPU freq/1024
+    ldi temp, 0b00000001
+    sts TIMSK0, temp            ; set the Timer Overflow Interrupt Enable (TOIE0) bit
+                                ; of the Timer Interrupt Mask Register (TIMSK0)
+
+    sei                         ; enable global interrupts
+
+Start:
+	rjmp Start
+
+TimerOverflow:
+    inc overflow_counter
+    cpi overflow_counter, 61
+    brne return                 ; go to return if not equal
+
+    clr overflow_counter
+
+    ldi temp, 0b00100000        ; toggle the LED
+    in led_status, PortB
+    eor led_status, temp
+    out PortB, led_status
+
+return:
+    reti
