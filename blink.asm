2b6fdf73b5e5d1eefb070699e1132336a22184c6
David Blurton <david.blurton@gmail.com>
2019-10-23 23:57:07 +0000

    Use constants from include file instead of binary

--- blink.asm
+++ blink.asm
@@ -7,6 +7,8 @@
 .def overflow_counter = r17
 .def led_status = r18
 
+.equ OVERFLOW_COUNT = 61        ; (16000000/1024)/(255*61) ~ 1Hz
+
 .org 0000
 rjmp Setup
 
@@ -14,14 +16,14 @@ rjmp Setup
 rjmp TimerOverflow
 
 Setup:
-	ldi temp, 0b00100000
+	ldi temp, (1 << PORTB5)
 	out DDRB, temp              ; set PB5 to output
 
-    ldi temp,  0b00000101
+    ldi temp, (1 << CS00) | (1 << CS02)
     out TCCR0B, temp            ; set the Clock Selector Bits CS00, CS01, CS02 to 101
                                 ; this puts Timer Counter0, TCNT0 in to FCPU/1024 mode
                                 ; so it ticks at the CPU freq/1024
-    ldi temp, 0b00000001
+    ldi temp, (1 << TOIE0)
     sts TIMSK0, temp            ; set the Timer Overflow Interrupt Enable (TOIE0) bit
                                 ; of the Timer Interrupt Mask Register (TIMSK0)
 
@@ -32,12 +34,12 @@ Start:
 
 TimerOverflow:
     inc overflow_counter
-    cpi overflow_counter, 61
+    cpi overflow_counter, OVERFLOW_COUNT
     brne return                 ; go to return if not equal
 
     clr overflow_counter
 
-    ldi temp, 0b00100000        ; toggle the LED
+    ldi temp, (1 << PORTB5)     ; toggle the LED
     in led_status, PortB
     eor led_status, temp
     out PortB, led_status
