4427dc947bcaff2ee8f84cb7bd1ccea8ec16ecc5
David Blurton <david.blurton@gmail.com>
2019-10-24 19:20:44 +0000

    Use 16 bit timer

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ all:
 	# avr-as -mmcu=atmega328p blink.asm
 
 flash: all
-	avrdude -p m328p -c stk500v1 -b 57600 -P /dev/cu.wchusbserial1420 -U flash:w:blink.hex
+	avrdude -p m328p -c stk500v1 -b 57600 -P /dev/cu.wchusbserial1410 -U flash:w:blink.hex
 
 dump:
 	avr-objdump -m avr5 -D blink.hex
--- blink.asm
+++ blink.asm
@@ -4,41 +4,27 @@
 .include "./m328Pdef.inc"
 
 .def temp = r16
-.def overflow_counter = r17
+.def port_mask = r17
 .def led_status = r18
 
-.equ OVERFLOW_COUNT = 61        ; (16000000/1024)/(255*61) ~ 1Hz
-
-.org 0000
 Setup:
-    ldi temp, (1 << PORTB5)
-    out DDRB, temp              ; set PB5 to output
+    ldi temp, (1 << CS12)           ; set the prescaler to 1/256
+    sts TCCR1B, temp
 
-    ldi temp, (1 << CS00) | (1 << CS02)
-    out TCCR0B, temp            ; set the Clock Selector Bits CS00, CS01, CS02 to 101
-                                ; this puts Timer Counter0, TCNT0 in to FCPU/1024 mode
-                                ; so it ticks at the CPU freq/1024
-    ldi temp, (1 << TOIE0)
-    sts TIMSK0, temp            ; set the Timer Overflow Interrupt Enable (TOIE0) bit
-                                ; of the Timer Interrupt Mask Register (TIMSK0)
+    ldi temp, (1 << TOIE1)          ; set the Timer Overflow Interrupt Enable (TOIE1) bit
+    sts TIMSK1, temp                ; of the Timer Interrupt Mask Register (TIMSK1)
 
-    sei                         ; enable global interrupts
+    ldi port_mask, (1 << PORTB5)    ; set PB5 to output
+    out DDRB, port_mask
+
+    sei                             ; enable global interrupts
 
 Start:
     rjmp Start
 
-.org 0x0020
+.org OVF1addr
 TimerOverflow:
-    inc overflow_counter
-    cpi overflow_counter, OVERFLOW_COUNT
-    brne return                 ; go to return if not equal
-
-    clr overflow_counter
-
-    ldi temp, (1 << PORTB5)     ; toggle the LED
-    in led_status, PortB
-    eor led_status, temp
+    eor led_status, port_mask
     out PortB, led_status
 
-return:
     reti
