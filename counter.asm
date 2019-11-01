44d98e0893684c9edf7269531fc1aab6a80d69b9
David Blurton <david.blurton@gmail.com>
2019-11-01 21:40:55 +0000

    Add counter example

---
+++ counter.asm
@@ -0,0 +1,30 @@
+;counter.asm
+;  an 8-bit binary counter
+
+.include "./m328Pdef.inc"
+
+.def temp = r16
+.def port_mask = r17
+.def counter = r18
+
+Setup:
+    ldi temp, (1 << CS11)           ; set the prescaler to 1/8
+    sts TCCR1B, temp
+
+    ldi temp, (1 << TOIE1)          ; set the Timer Overflow Interrupt Enable (TOIE1) bit
+    sts TIMSK1, temp                ; of the Timer Interrupt Mask Register (TIMSK1)
+
+    ldi port_mask, 0b11111111       ; set all PortB to output
+    out DDRB, port_mask
+
+    sei
+
+Start:
+    rjmp Start
+
+.org OVF1addr
+TimerOverflow:
+    inc counter
+    out PortB, counter
+
+    reti
